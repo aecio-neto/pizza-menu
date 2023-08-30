98e4996c66f76d87029031c16849484503a85897
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2023-08-30 08:52:03 -0300

    add assets

---
+++ public/data.js
@@ -0,0 +1,44 @@
+const pizzaData = [
+  {
+    nome: "Focaccia",
+    ingredientes: "Pão com azeite de oliva italiano e alecrim",
+    preço: 6,
+    nomeFoto: "pizzas/focaccia.jpg",
+    esgotado: false,
+  },
+  {
+    nome: "Pizza Margherita",
+    ingredientes: "Tomate e mozarela",
+    preço: 10,
+    nomeFoto: "pizzas/margherita.jpg",
+    esgotado: false,
+  },
+  {
+    nome: "Pizza Spinaci",
+    ingredientes: "Tomate, mozarela, espinafre e queijo ricota",
+    preço: 12,
+    nomeFoto: "pizzas/spinaci.jpg",
+    esgotado: false,
+  },
+  {
+    nome: "Pizza Funghi",
+    ingredientes: "Tomate, mozarela, cogumelos e cebola",
+    preço: 12,
+    nomeFoto: "pizzas/funghi.jpg",
+    esgotado: false,
+  },
+  {
+    nome: "Pizza Salamino",
+    ingredientes: "Tomate, mozarela e pepperoni",
+    preço: 15,
+    nomeFoto: "pizzas/salamino.jpg",
+    esgotado: true,
+  },
+  {
+    nome: "Pizza Prosciutto",
+    ingredientes: "Tomate, mozarela, presunto, rúcula e queijo burrata",
+    preço: 18,
+    nomeFoto: "pizzas/prosciutto.jpg",
+    esgotado: false,
+  },
+];
