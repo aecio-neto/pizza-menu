13dfef3df70f4b60cfb6513033e63cbed869cc32
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2023-08-30 08:45:30 -0300

    upload version 1.0.0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,163 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Pão com azeite de oliva italiano e alecrim",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Margherita",
+    ingredients: "Tomate e mozarela",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Espinafre",
+    ingredients: "Tomate, mozarela, espinafre e queijo ricota",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomate, mozarela, cogumelos e cebola",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salaminho",
+    ingredients: "Tomate, mozarela e pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Burrata",
+    ingredients: "Tomate, mozarela, presunto, rúcula e queijo burrata",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function App() {
+  return (
+    <div className='container'>
+      <Header />
+      <Menu />
+      <Button />
+      <Footer />
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <header className='header'>
+      <h1>React Pizza!</h1>
+    </header>
+  )
+}
+
+function Menu() {
+  const pizzas = pizzaData
+  // const pizzas = []
+  const numPizza = pizzas.length
+
+  return (
+    <main className='menu'>
+      <h2 className=''>Cardápio</h2>
+
+
+      { numPizza > 0 ? (
+        <>
+          <p>Cozinha Italiana autêntica. 6 pratos criativos para escolher. Feito no nosso forno de pedra, tudo orgânico, tudo delicioso.</p>
+          
+          <ul className='pizzas'>
+            {pizzaData.map((pizza) => {
+              return <Pizza pizzaObj={pizza} key={pizza.name} />
+            })}
+          </ul>
+        </>
+      ) : ( <p>Ainda estamos trabalhando no nosso cardápio. Volte em breve.</p> )}
+      
+      { /* Componentes utilizados de forma "manual"
+          <Pizza name="Pizza Spinaci" ingredients="Tomante, Espinafre" price="56" photoName="pizzas/spinaci.jpg" />
+          <Pizza name="Pizza Margherita" ingredients="Tomante, Manjericão" price="50" photoName="pizzas/margherita.jpg" /> 
+      */}
+    
+    </main>
+  )
+}
+
+function Pizza ({ pizzaObj }) {
+  const {name, photoName, ingredients, price, soldOut } = pizzaObj
+
+  return (
+
+    <li className={soldOut ? 'pizza sold-out' : 'pizza'} >
+      <img src={photoName} alt={name} />              
+      <div>
+        <h3>{name}</h3>
+        <p>Ingredientes: {ingredients}.</p>
+        <span>{soldOut ? 'ESGOTADO' : price}</span>
+      </div>
+    </li>
+  )
+}
+
+// Primeira versão do componente Pizza (um certo malabarismo)
+
+// function Pizza2 () {
+//   const pizzas = pizzaData.map((pizza) => {
+//     return (
+//         <li key={pizza.name}>
+//           <div className='pizza'>
+//             <img src={`./${pizza.photoName}`} alt={pizza.name} />              
+//               <div>
+//                 <h3>{pizza.name}</h3>
+//                 <p>Ingredientes: {pizza.ingredients}</p>
+//                 <span>{pizza.price}</span>
+//               </div>
+//           </div>
+//         </li>
+//     )
+//   })
+//   return <ul className='pizzas'>{pizzas}</ul>
+// }
+
+function Button() {
+  return <button className='btn'>Fazer Pedido</button>
+}
+
+function Footer() {
+  const openHour = 10;
+  const closeHour = 23;
+  const hour = new Date().getHours();
+  const isOpen = hour >= openHour && hour < closeHour
+  const message = isOpen 
+  ? 'Estamos abertos. Venha nos visitar ou faça o seu pedido online!' 
+  : 'Horário de funcionamento: 18 às 23 horas'
+
+  return (
+    <footer className='footer'>
+      <h3>{message}</h3>
+    </footer> 
+  ) 
+}
+
+
+const root = ReactDom.createRoot(document.getElementById("root"))
+root.render(<App />)
+
+// Antes do react18 
+
+// ReactDOM.render(<App />, document.getElementById("root "))
